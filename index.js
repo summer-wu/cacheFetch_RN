ca5a8cdff8893beabf92fbcf81b4b167c62a10f8
giveme5 <286674783@qq.com>
2019-10-27 21:46:31 +0800

    remove log ,support headersPopulator

--- index.js
+++ index.js
@@ -30,13 +30,13 @@ const CacheKeysListManager = {
     let keysList = [];
     try {
       const keysListStr = await AsyncStorage.getItem(KEY_fetchCacheKeysList);
-      log('keysListStr:' + keysListStr);
+      // log('keysListStr:' + keysListStr);
       if (_.isString(keysListStr)) {
         keysList = JSON.parse(keysListStr);
         assert(_.isArray(keysList));
       }
     } catch (e) {}
-    log('will set latestKeysList to' + keysList);
+    // log('will set latestKeysList to' + keysList);
     CacheKeysListManager.latestKeysList = keysList;
     return keysList;
   },
@@ -57,7 +57,7 @@ const CacheKeysListManager = {
       }
       return await CacheKeysListManager.saveCacheKeysList(keysList);
     } else if (action === 'delete') {
-      _.remove(keysList, x => x === key);
+      _.remove(keysList, (x) => x === key);
       return await CacheKeysListManager.saveCacheKeysList(keysList);
     } else if (action === 'clear') {
       await AsyncStorage.multiRemove(keysList);
@@ -75,7 +75,7 @@ const Converter = {
     const clonedOne = response.clone();
     const text = await clonedOne.text(); //may throw in text()
     const headersObj = Converter.objectFromHeaders(response.headers);
-    const responseObj = {text, headersObj};
+    const responseObj = { text, headersObj };
 
     const responseStr = JSON.stringify(responseObj);
     return responseStr;
@@ -83,7 +83,7 @@ const Converter = {
 
   responseFromString(responseStr: string): Response {
     const responseObj = JSON.parse(responseStr);
-    const {text, headersObj} = responseObj;
+    const { text, headersObj } = responseObj;
     const headers = Converter.headersFromObject(headersObj);
     const response = new Response(text, headers);
     return response;
@@ -111,8 +111,23 @@ function calcCacheKey(input, init): string {
   assert(_.isString(input));
   const method = _.get(init, 'method', 'GET');
   const headers = _.get(init, 'headers');
+  let headersSum = null;
+  if (_.isObject(headers)) {
+    let headersObj = null;
+    if (_.isFunction(headers.entries)) {
+      //Headers
+      headersObj = Converter.objectFromHeaders(headers); // a new object
+    } else {
+      headersObj = _.clone(headers); // a new object
+    }
+
+    const headersObjAfterPopulate = calcCacheKey.headersPopulator(headersObj);
+    headersSum = sum(headersObjAfterPopulate); //plain object
+  } else {
+    headersSum = String(headers);
+  }
   const body = _.get(init, 'body');
-  const key = `${method}_${input}_${sum(headers)}_${sum(body)}`;
+  const key = `${method}_${input}_${headersSum}_${sum(body)}`;
   return key;
 }
 
@@ -133,14 +148,13 @@ async function getCacheValueWithKey(key) {
 
 //save to disk
 async function saveCacheWithKeyValue(key, value) {
-  log('saveCacheWithKeyValue,key=' + key);
   assert(_.isString(key));
   assert(_.isString(value));
 
   try {
     await AsyncStorage.setItem(key, value);
     await CacheKeysListManager.updateCacheKeysList('append', key);
-    log('saveCacheWithKeyValue key=', key, 'success', 'value:', value);
+    log(`saveCacheWithKeyValue key=${key} success`);
   } catch (e) {
     log('saveCacheItem error', e);
   }
@@ -148,7 +162,8 @@ async function saveCacheWithKeyValue(key, value) {
 
 function log(...msg) {
   const prefix = 'cacheFetch_RN ';
-  logger.log(prefix, ...msg);
+  const msgStr = prefix + msg.join(' ');
+  logger.log(msgStr);
 }
 
 // cacheFetch is just a enhanced fetch,it's signature is same with fetch：Promise<Response> fetch(input[, init]);
@@ -157,7 +172,6 @@ function cacheFetch(input, init = {}): Promise {
   assert(_.isString(input), 'input only accept string，dont support Request');
   const cacheKey = calcCacheKey(input, init);
   const keyExists = CacheKeysListManager.isKeyExistsInLatestKeysList(cacheKey);
-
   let shouldUseCache = null;
   if (keyExists) {
     const vetoResult = cacheFetch.veto(input, init);
@@ -165,12 +179,11 @@ function cacheFetch(input, init = {}): Promise {
   } else {
     shouldUseCache = false;
   }
-
   if (shouldUseCache) {
     //cache exists
-    log('cache exists,cacheKey=', cacheKey);
+    log('cache exists,cacheKey=' + cacheKey);
     return new Promise((resolve, reject) => {
-      getCacheValueWithKey(cacheKey).then(cacheValue => {
+      getCacheValueWithKey(cacheKey).then((cacheValue) => {
         const response = Converter.responseFromString(cacheValue);
         resolve(response);
       });
@@ -178,18 +191,15 @@ function cacheFetch(input, init = {}): Promise {
   } else {
     //cache not exists,need to fetch
     // fetch().then() return a Promise
-    log('cache not exists! cacheKey=', cacheKey);
-    return fetch(input, init).then(response => {
+    log('cache not exists! cacheKey=' + cacheKey);
+    return cacheFetch.fetch(input, init).then((response) => {
       if (response.ok) {
         Converter.stringFromReponse(response)
-          .then(responseStr => {
-            log('will saveCacheWithKeyValue' + cacheKey);
+          .then((responseStr) => {
             saveCacheWithKeyValue(cacheKey, responseStr);
           })
-          .catch(reason => {
-            log(
-              'stringFromReponse fail,it seems that response.body cannot converto text',
-            );
+          .catch((reason) => {
+            log('stringFromReponse fail,it seems that response.body cannot converto text');
           });
       }
       return response;
@@ -208,19 +218,36 @@ const defaultVeto = (input, init) => {
   return true; //always use cache
 };
 
+// you can decide which header fields to use when calcCacheKey
+// default use all fields
+// you can remove some field, ie Cookie/ETag/If-Modified-Since
+const defaultHeadersPopulator = (headers) => {
+  //headers is a plain object
+  assert(_.isObject(headers));
+  assert(_.isUndefined(headers.entries));
+  debugger;
+  return headers;
+};
+
 // high order function,return cacheFetch
+// headersPopulator is only used when calcCacheKey
 // veto is a function: boolean veto(input,init)，return true means allow using cache，false means do not allow using cache
 // logger0 is a object,it contains a log function，defaults to console
 // return a function called cacheFetch
-export function makeCacheFetch(
-  AsyncStorage,
-  veto = defaultVeto,
-  logger0 = console,
-) {
+export function makeCacheFetch({
+                                 AsyncStorage,
+                                 fetch,
+                                 headersPopulator = defaultHeadersPopulator,
+                                 veto = defaultVeto,
+                                 logger0 = console,
+                               }) {
   assert(_.isFunction(veto));
   assert(_.isFunction(logger0.log));
 
   setAsyncStorage(AsyncStorage);
+  cacheFetch.fetch = fetch; //underlaying fetch function
+  calcCacheKey.headersPopulator = headersPopulator;
+
   cacheFetch.veto = veto;
   logger = logger0;
   return cacheFetch;
